5f8003cd39615558baa45ca3b791f92841ef6c9a
Rainer Wittmann <R.Wittmann@spirit.de>
2013-12-11 09:05:28 -0800

    update directories for bower. update jade views

--- public/app/main.js
+++ public/app/main.js
@@ -1,12 +1,12 @@
 ﻿requirejs.config({
     paths: {
-        'text': '../lib/require/text',
+        'text': '../lib/requirejs-text/text',
         'durandal':'../lib/durandal/js',
         'plugins' : '../lib/durandal/js/plugins',
         'transitions' : '../lib/durandal/js/transitions',
-        'knockout': '../lib/knockout/knockout-2.3.0',
+        'knockout': '../lib/knockout.js/knockout',
         'bootstrap': '../lib/bootstrap/js/bootstrap',
-        'jquery': '../lib/jquery/jquery-1.9.1'
+        'jquery': '../lib/jquery/jquery'
     },
     shim: {
         'bootstrap': {
